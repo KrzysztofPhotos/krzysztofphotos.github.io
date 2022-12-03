7a4576d84ec6df530e9e62bada2941211712f954
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-03 09:05:41 +0100

    Dalsze zmiany

--- multiplication_app/script.js
+++ multiplication_app/script.js
@@ -7,23 +7,26 @@ const inputEl = document.getElementById("input")
 
 const formEl = document.getElementById("form")
 
-questionEl.innerText = "What is "+ num1 + " multiply by " + num2
-
 let score = 0;
 
+questionEl.innerText = "What is "+ num1 + " multiply by " + num2;
 // I could type short version 
-// questionEl.innerText = `What is ${num1} multiply by ${num2}`
+// questionEl.innerText = `What is ${num1} multiply by ${num2}`;
 
-const correctAnswer = num1 * num2
+const correctAnswer = num1 * num2;
 
 formEl.addEventListener("submit", ()=>{
-    const userAns = +inputEl.value
+    const userAns = +inputEl.value;
     if(userAns == correctAnswer){
-        score ++
-        console.log(score)
+        score ++;
+        console.log(score);
     }
     else {
-        score --
-        console.log(score)
+        score --;
+        console.log(score);
     }
 })
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score));
+}
