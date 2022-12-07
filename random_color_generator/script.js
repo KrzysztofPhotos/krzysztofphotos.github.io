014de859b0714ee51973a262928ca7399ec2c502
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-07 08:14:51 +0100

    Save 8:14

--- random_color_generator/script.js
+++ random_color_generator/script.js
@@ -1,1 +1,26 @@
 const containerEl = document.querySelector(".container");
+
+for (let index = 0; index < 30; index++) {
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl)
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container")
+
+console.log(colorContainerEls)
+
+function generateColors(){
+    
+}
+
+function randomColor(){
+    const chars = "0123456789abcdef";
+    const colorCodeLenght = 6;
+    let colorCode = "";
+    for (let index = 0; index < colorCodeLenght; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        colorCode += chars.substring(randomNum, randomNum + 1)
+        console.log(colorCode, randomNum)
+    }
+}
