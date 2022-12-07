9792c2dd41138e9b3e947f7546450c965b95550a
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-07 18:32:53 +0100

    Save 18:32

---
+++ windows_login_page/script.js
@@ -0,0 +1,9 @@
+function keyWasPressed(){
+    const inputEl = document.querySelector(".input-pin");
+    let inputLen = inputEl.lenght;
+    console.log(inputLen);
+
+    if (inputEl == "3") {
+        console.log("great")
+    }
+}
