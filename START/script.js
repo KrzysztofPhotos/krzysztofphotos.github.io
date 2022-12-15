befcaef3d45c35d08140a6d92cefc67e80fd1753
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-15 07:25:21 +0100

    100% working the navbar

--- START/script.js
+++ START/script.js
@@ -1,1 +1,15 @@
 // TODO animowany tekst (title) strony w js zmieniający się tekst
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",  () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
