7a0a17e899806cc887a87e95950b34eed901baff
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-18 18:37:49 +0100

    miejsca na ikonki

--- windows_desktop/script.js
+++ windows_desktop/script.js
@@ -1,9 +1,5 @@
 let taskbarEl = document.querySelector('.taskbar-icon');
 let height = taskbarEl.offsetHeight;
-console.log(height);
-console.log("elo");
-taskbarEl.style.width = height + "px";
-
 document.getElementById("taskApp1").style.width = height + "px";
 document.getElementById("taskApp2").style.width = height + "px";
 document.getElementById("taskApp3").style.width = height + "px";
@@ -11,3 +7,21 @@ document.getElementById("taskApp4").style.width = height + "px";
 document.getElementById("taskApp5").style.width = height + "px";
 document.getElementById("taskApp6").style.width = height + "px";
 document.getElementById("taskApp7").style.width = height + "px";
+
+
+let columIcon = document.querySelector('.icon-el');
+let heightColumnEl = columIcon.offsetHeight;
+document.getElementById("icon1").style.width = heightColumnEl + "px";
+document.getElementById("icon2").style.width = heightColumnEl + "px";
+document.getElementById("icon3").style.width = heightColumnEl + "px";
+document.getElementById("icon4").style.width = heightColumnEl + "px";
+document.getElementById("icon5").style.width = heightColumnEl + "px";
+document.getElementById("icon6").style.width = heightColumnEl + "px";
+document.getElementById("icon7").style.width = heightColumnEl + "px";
+document.getElementById("icon8").style.width = heightColumnEl + "px";
+document.getElementById("icon9").style.width = heightColumnEl + "px";
+document.getElementById("icon10").style.width = heightColumnEl + "px";
+document.getElementById("icon11").style.width = heightColumnEl + "px";
+document.getElementById("icon12").style.width = heightColumnEl + "px";
+document.getElementById("icon13").style.width = heightColumnEl + "px";
+document.getElementById("icon14").style.width = heightColumnEl + "px";
