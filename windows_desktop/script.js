aebb373558a01b394f7e1aedb8ca5d9571c69297
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-18 16:17:48 +0100

    divy zmieniają swój rozmiar względem wysokości

---
+++ windows_desktop/script.js
@@ -0,0 +1,13 @@
+let taskbarEl = document.querySelector('.taskbar-icon');
+let height = taskbarEl.offsetHeight;
+console.log(height);
+console.log("elo");
+taskbarEl.style.width = height + "px";
+
+document.getElementById("taskApp1").style.width = height + "px";
+document.getElementById("taskApp2").style.width = height + "px";
+document.getElementById("taskApp3").style.width = height + "px";
+document.getElementById("taskApp4").style.width = height + "px";
+document.getElementById("taskApp5").style.width = height + "px";
+document.getElementById("taskApp6").style.width = height + "px";
+document.getElementById("taskApp7").style.width = height + "px";
