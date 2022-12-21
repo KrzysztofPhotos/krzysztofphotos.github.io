abcb4b7cb8bbd9b2d3bf41ee0f913b389688f768
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-21 21:40:48 +0100

    it doesn't work

---
+++ image_slider/script.js
@@ -0,0 +1,21 @@
+const nextEl = document.querySelector(".next");
+
+const imgsEl = document.querySelectorAll(".img");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+let currectImg = 1;
+
+nextEl.addEventListener("click", () => {
+    currectImg++;
+    updateImg();
+});
+
+function updateImg(){
+    if (currectImg > imgsEl.length){
+        currectImg = 1;
+    }
+    imageContainerEl.style.transform = `translateX(-${(currectImg - 1) * 1000}px)`;
+}
+
+const prevEl = document.querySelector(".prev");
