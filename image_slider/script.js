ba25194024f7250f5d63dc88170942d7038d7ec1
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-22 06:48:26 +0100

    ${variable} JS

--- image_slider/script.js
+++ image_slider/script.js
@@ -1,6 +1,14 @@
 const nextEl = document.querySelector(".next");
 
+const imageContainerEl = document.querySelector(".image-container");
+
+let currectImg = 1;
 
 nextEl.addEventListener("click", () => {
-    
+    currectImg++;
+    updateImg();
 })
+
+function updateImg(){
+    imageContainerEl.style.transform = `translateX(-${(currectImg - 1) * 1000}px)`
+}
