404e2c7a04be837d74ecbaebfa75736a6756bdf0
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-29 06:11:38 +0100

    animation opacity

--- steam_free_key_generator/script.js
+++ steam_free_key_generator/script.js
@@ -1,5 +1,26 @@
 const clicked = document.getElementById("button");
 
+
+
 clicked.addEventListener("click", () => {
-    console.log("clciked")
+    console.log("clciked");
+    const toHide1 = document.querySelector(".cont-img");
+    const toHide2 = document.querySelector(".cont_text");
+    const toHide3 = document.querySelector(".cont-select");
+    const toHide4 = document.querySelector(".przycisk");
+    hide(toHide1);
 })
+
+function wysun(i, elementToMove){
+    var opacity_num = 1.00 + (i-1)*(-0.02);
+    elementToMove.style.opacity = opacity_num;
+}
+
+function hide(containerEl){
+    for (let i = 1; i <= 50; i++) {
+        setTimeout(() => {
+            wysun(i, containerEl);
+        }, 8 * i);
+    }
+    setTimeout(() => {containerEl.classList.add("hide-el"); }, 200);
+}
