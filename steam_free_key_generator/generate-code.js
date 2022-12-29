4b6058c1de16f4b5ed41f6653affe2b8f37a1a22
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-29 18:03:41 +0100

    all done

--- steam_free_key_generator/generate-code.js
+++ steam_free_key_generator/generate-code.js
@@ -1,27 +1,21 @@
-function random_codes(length)
-    {
+var code = "";
+    function random_codes(length){
         var characters = 'ABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789';
         var random_string = '';
-        if(length>0)
-        {
-            for(var i=0; i<length; i++)
-            {
+        if(length>0){
+            for(var i=0; i<length; i++){
                 random_string += characters.charAt(Math.floor(Math.random() * characters.length));
             }
         }
         return random_string;
     }
-    function write_random_code()
-    {
-        for(var x=0; x<4; x++)
-        {
-            document.write(random_codes(5) + "-");
-        }
-        document.write(random_codes(5));
+function write_random_code(){
+    for(var x=0; x<2; x++){
+        code += random_codes(5) + "-";
     }
-    for(x=0; x<5; x++)
-    {
-        write_random_code();
-        document.write("<p>")
-        
-    }
+    code += random_codes(5);
+    document.querySelector("#paste-code").innerHTML = code;
+}
+
+write_random_code();
+
