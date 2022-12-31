3920fea8ef9f7014c5fe3fed9c31621260cd64ef
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-31 12:17:28 +0100

    sound working in all buttons

--- maze_bank/script.js
+++ maze_bank/script.js
@@ -1,9 +1,30 @@
 // SOUND WHEN CLICKED
 document.body.addEventListener("click", () => {
     console.log("clicked");
-    play();
+    document.getElementById('clicksfx').play();
 })
 
-function play() {
-    document.getElementById('clicksfx').play();
-  }
+function hovered(){
+    console.log('hover');
+    document.getElementById('hoversfx').play();
+}
+
+document.querySelector(".btn").addEventListener("mouseover", () => {
+    hovered();
+})
+
+document.querySelector(".left-arrow").addEventListener("mouseover", () => {
+    hovered();
+})
+
+document.querySelector(".home-btn").addEventListener("mouseover", () => {
+    hovered();
+})
+
+document.querySelector(".clock").addEventListener("mouseover", () => {
+    hovered();
+})
+
+document.querySelector(".search-bar-x").addEventListener("mouseover", () => {
+    hovered();
+})
