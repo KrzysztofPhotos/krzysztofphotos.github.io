ec16ecfcef17dc8705017c16d82ca91bbace8af1
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-31 12:09:39 +0100

    sound working when clicked

--- maze_bank/script.js
+++ maze_bank/script.js
@@ -1,4 +1,9 @@
 // SOUND WHEN CLICKED
 document.body.addEventListener("click", () => {
-    console.log("clicked")
+    console.log("clicked");
+    play();
 })
+
+function play() {
+    document.getElementById('clicksfx').play();
+  }
